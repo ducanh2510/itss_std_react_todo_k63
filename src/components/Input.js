3007bad9bdc8243bda591dfcc39442f7665db9d6
coderx4032 <>
2022-05-31 06:59:40 +0000

    Create by  another component

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
 　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
 */
-function Input(  ) {
+function Input() {
 
   return (
     <div className="panel-block">
